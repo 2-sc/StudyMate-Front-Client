f953ae3d732dc3a0542678ea21b811891e229a11
ujin <dbwlsxkal95@gmail.com>
2022-10-04 14:13:19 +0900

    :construction:(feature/ujin/#8): Router 경로 설정

--- studymate/src/App.jsx
+++ studymate/src/App.jsx
@@ -1,7 +1,23 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import StopWatch from "./pages/stopwatch/StopWatch";
+import Planner from "./pages/planner/Planner";
+import Log from "./pages/log/Log";
 
 function App() {
-  return <div className="App">upstream Deploy test</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/stopwatch" element={<StopWatch />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="/log" element={<Log />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ studymate/src/pages/log/Log.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Log() {
+  return <div>Log</div>;
+}
+
+export default Log;
