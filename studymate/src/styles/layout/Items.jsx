2c7db215bdcdd70df4d286786b6a321849d6fb5c
ujin <dbwlsxkal95@gmail.com>
2022-10-11 00:42:54 +0900

    :package::fire:(feature/ujin/#10): react icon 라이브러리 삭제 후 기존 아이콘으로 스타일 변경

--- studymate/src/styles/layout/Items.jsx
+++ studymate/src/styles/layout/Items.jsx
@@ -1,43 +1,62 @@
-import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import styled from "@emotion/styled";
 import {
-  BsHouseDoorFill,
-  BsStopwatchFill,
-  BsFillCalendarCheckFill,
-  BsShareFill,
-} from "react-icons/bs";
-
-const itemList = [
-  {
-    icon: <BsHouseDoorFill />,
-    name: "Home",
-  },
-  {
-    icon: <BsStopwatchFill />,
-    name: "Stopwatch",
-  },
-  {
-    icon: <BsFillCalendarCheckFill />,
-    name: "Planner",
-  },
-  {
-    icon: <BsShareFill />,
-    name: "Log",
-  },
-];
+  faHouse,
+  faStopwatch,
+  faList,
+  faShareNodes,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Items() {
+  const pathName = useLocation().pathname;
+
+  const items = [
+    {
+      icon: <IconStyle icon={faHouse} />,
+      name: "Home",
+      path: "/",
+    },
+    {
+      icon: <IconStyle icon={faStopwatch} />,
+      name: "Stopwatch",
+      path: "/stopwatch",
+    },
+    {
+      icon: <IconStyle icon={faList} />,
+      name: "Planner",
+      path: "/planner",
+    },
+    {
+      icon: <IconStyle icon={faShareNodes} />,
+      name: "Log",
+      path: "/log",
+    },
+  ];
+
   return (
-    <Category>
-      {itemList.map((item, idx) => (
-        <Item key={`${idx}`} icon={item.icon} title={item.name} />
+    <>
+      {items.map((item, idx) => (
+        <Link to={item.path} style={{ textDecoration: "none" }}>
+          <Item
+            key={idx}
+            icon={item.icon}
+            title={item.name}
+            isActive={pathName === item.path ? true : false}
+          />
+        </Link>
       ))}
-    </Category>
+    </>
   );
 }
 
-function Item({ icon, title }) {
-  return (
+function Item({ icon, title, isActive }) {
+  return isActive === true ? (
+    <ListActive>
+      <Icon> {icon}</Icon>
+      {title}
+    </ListActive>
+  ) : (
     <List>
       <Icon> {icon}</Icon>
       {title}
@@ -47,27 +66,30 @@ function Item({ icon, title }) {
 
 export default Items;
 
-const Category = styled.ul`
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-`;
-
-const List = styled.li`
+const List = styled.div`
   height: 40px;
   margin-left: 10px;
   margin-bottom: 15px;
   display: flex;
   align-items: center;
   list-style: none;
+
   color: #646464;
   &:hover {
-    background: rgba(228, 187, 255, 0.3);
-    border-radius: 22px 0 0 22px;
+    color: rgba(228, 187, 255, 0.3);
+    transition: all 0.7s;
   }
+`;
+
+const ListActive = styled(List)`
+  background: rgba(228, 187, 255, 0.3);
+  border-radius: 22px 0 0 22px;
+  color: #646464 !important;
 `;
 
 const Icon = styled.div`
   padding-left: 10px;
   margin-right: 14px;
 `;
+
+const IconStyle = styled(FontAwesomeIcon)``;
