b4313fca89d6e4a16d52a7373084558b9e200160
ujin <dbwlsxkal95@gmail.com>
2022-10-11 04:07:23 +0900

    :lipstick:(feature/ujin/#22): 플래너 큰 틀 UI 구현

--- studymate/src/pages/planner/Planner.jsx
+++ studymate/src/pages/planner/Planner.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import Box from '../../components/common/Box';
+import Calendar from '../../components/calendar/Calendar';
+import Dazim from './components/Dazim';
 
 function Planner() {
-  return <div>Planner</div>;
+  return (
+    <Wrapper>
+      <RightBox>
+        <TopBox size={[438, 53]}>
+          <Dazim />
+        </TopBox>
+        <Container size={[438, 400]}></Container>
+      </RightBox>
+      <LeftBox>
+        <Calendar />
+        <SideBox size={[215, 220]}></SideBox>
+      </LeftBox>
+    </Wrapper>
+  );
 }
 
 export default Planner;
+
+const Wrapper = styled.div`
+  width: calc(100% - 215px);
+  display: flex;
+  justify-content: space-around;
+  margin-top: 10px;
+`;
+
+const RightBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+`;
+const TopBox = styled(Box)`
+  border-radius: 25px;
+  margin-bottom: 25px;
+`;
+
+const Container = styled(Box)``;
+
+const LeftBox = styled.div``;
+
+const SideBox = styled(Box)`
+  margin-top: 25px;
+`;
