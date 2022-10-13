51bb1244f031a064e70563e356bb276a4463d86e
halamlee <halam.lee.lami@gmail.com>
2022-10-14 02:44:50 +0900

    :sparkles:(feature/halam/#30): 버튼 눌러서 그래프 전환하기

--- studymate/src/components/graph/Graph.jsx
+++ studymate/src/components/graph/Graph.jsx
@@ -1,43 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '../common/Box';
 import styled from '@emotion/styled';
 import PieGraph from './PieGraph';
+import TimeLineGraph from './TimeLineGraph';
 
 function Graph({ data }) {
-  let total = 0;
+  const [graphState, setGraphState] = useState('timeline');
 
-  data.forEach(el => {
-    total = total + el.value;
-  });
+  const getColor = name => {
+    return name.name === graphState ? '#e7e7e7' : 'inherit';
+  };
 
-  const totalHours = Math.floor(total / 60);
-
-  const totalMinutes = Math.floor(total % 60);
-
-  const totalSecondes = Math.floor(totalMinutes % 60);
-
-  const hoursStr = totalHours < 10 ? `0${totalHours}` : `${totalHours}`;
-  const minutesStr = totalMinutes < 10 ? `0${totalMinutes}` : `${totalMinutes}`;
-  const secondesStr = totalSecondes < 10 ? `0${totalSecondes}` : `${totalSecondes}`;
+  const handleGraphState = e => {
+    setGraphState(e.target.getAttribute('name'));
+    getColor(e.target.getAttribute('name'));
+  };
 
   return (
     <Wrapper size={[475, 439]} opacity={0.8}>
-      <PieGraph data={data} />
-      <Total>{`Total ${hoursStr} : ${minutesStr} : ${secondesStr}`}</Total>
+      <Title>Graph</Title>
+      <Buttons>
+        <Button name="timeline" graphState={graphState} getColor={getColor} onClick={handleGraphState}>
+          Time Line
+        </Button>
+        <Button name="total" graphState={graphState} getColor={getColor} onClick={handleGraphState}>
+          Total
+        </Button>
+      </Buttons>
+      {graphState === 'timeline' ? <TimeLineGraph /> : <PieGraph data={data} />}
     </Wrapper>
   );
 }
 
 export default Graph;
 
+const center = `
+display: flex;
+justify-content: flex-start;
+align-items: center;
+`;
+
 const Wrapper = styled(Box)`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
+  ${center}
   flex-direction: column;
 `;
 
-const Total = styled.h1`
-  font-size: 30px;
-  margin: -60px 0 0;
+const Title = styled.h1`
+  font-size: 20px;
+  margin: 15px 0;
+`;
+
+const Buttons = styled.div`
+  ${center};
+  gap: 80px;
+  z-index: 2;
+  margin-bottom: 10px;
+`;
+
+const Button = styled.div`
+  padding: 6px 30px;
+  background-color: ${props => props.getColor};
+  border-radius: 10px;
+  cursor: pointer;
 `;
