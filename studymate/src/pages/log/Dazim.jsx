8d92dc1a7e0e3d1ecd684ea482e440b8c1e12d88
halamlee <halam.lee.lami@gmail.com>
2022-10-25 15:30:34 +0900

    :lipstick:(halam/beta): 다짐 퍼블리싱

--- studymate/src/pages/log/Dazim.jsx
+++ studymate/src/pages/log/Dazim.jsx
@@ -3,7 +3,22 @@ import styled from '@emotion/styled';
 import Box from '../../components/common/Box';
 
 function Dazim() {
-  return <Box size={[438, 53]} opacity={0.8}></Box>;
+  return (
+    <BoxStyled size={[438, 53]} opacity={0.8}>
+      <Text>오늘 하루도 화이팅 🔥</Text>
+    </BoxStyled>
+  );
 }
 
 export default Dazim;
+
+const BoxStyled = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.h1`
+  margin: 0;
+  font-size: 24px;
+`;
